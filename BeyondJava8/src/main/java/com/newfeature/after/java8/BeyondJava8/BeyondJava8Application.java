90601d90acbe676c36d12c241b4f552abf2267f4
dkwork2727 <dk.work2727gmail.com>
2023-03-17 22:45:12 +0530

    Second commit

---
+++ BeyondJava8/src/main/java/com/newfeature/after/java8/BeyondJava8/BeyondJava8Application.java
@@ -0,0 +1,13 @@
+package com.newfeature.after.java8.BeyondJava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondJava8Application.class, args);
+	}
+
+}
---
+++ BeyondJava8/src/test/java/com/newfeature/after/java8/BeyondJava8/BeyondJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newfeature.after.java8.BeyondJava8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
